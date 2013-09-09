691fc2b06029ba3e1f2cd0075adc7e9f4d50683c
Deluan <deluan@thoughtworks.com>
2013-09-09 19:47:56 -0400

    finally some tests for the main function

--- test/treeinfo_spec.js
+++ test/treeinfo_spec.js
@@ -1,6 +1,73 @@
 var fs = require('fs');
-var util = require('../lib/treeinfo');
+var path = require('path');
+var sinon = require('sinon');
+var util = require('../lib/util');
+var treeinfo = require('../lib/treeinfo');
 
 describe('treeinfo', function () {
-    it('should be tested....');
+    var tree = [];
+
+    before(function () {
+        treeinfo._readTreeFile = sinon.stub().throws(new Error());
+        treeinfo._writeTreeFile = sinon.stub();
+        util.recurseTree = function(root, progress) {
+            tree.forEach(function (dir) {
+                progress(1, dir);
+            });
+            return tree;
+        }
+    });
+
+    beforeEach(function () {
+        tree = [];
+        treeinfo._is_valid = sinon.stub().returns(true);
+    });
+
+    it('matches by leaves', function() {
+        tree = [
+            '/bin',
+            '/usr/bin'
+        ];
+
+        treeinfo.find("bin").should.be.equal("/bin");
+    });
+
+    it('return empty string if path does not exists', function() {
+        tree = [
+            '/bin',
+            '/usr/bin'
+        ];
+        treeinfo._is_valid = sinon.stub().withArgs("/bin").returns(false);
+
+        treeinfo.find("bin").should.be.equal("");
+    });
+
+    it('matches next leaf, cyclic', function() {
+        tree = [
+            '/bin',
+            '/usr/bin'
+        ];
+        treeinfo.find("bin", "/bin").should.be.equal("/usr/bin");
+        treeinfo.find("bin", "/usr/bin").should.be.equal("/bin");
+    });
+
+    it('matches by breadcrumb', function() {
+        tree = [
+            '/bin',
+            '/usr/bin',
+            '/usr/local/bin'
+        ];
+        treeinfo.find("ulb").should.be.equal("/usr/local/bin");
+    });
+
+    it('gives priority to breadcrumb', function() {
+        tree = [
+            '/bin',
+            '/ulb',
+            '/usr/bin',
+            '/usr/local/bin'
+        ];
+        treeinfo.find("ulb").should.be.equal("/usr/local/bin");
+        treeinfo.find("ulb", "/usr/local/bin").should.be.equal("/ulb");
+    });
 });
